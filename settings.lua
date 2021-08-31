bed7c325a3890adfe050247db488f996a124930c
ZwerOxotnik <zweroxotnik@gmail.com>
2021-08-31 13:31:17 +0300

    Update to 0.6.2

--- settings.lua
+++ settings.lua
@@ -1,3 +1,3 @@
 data:extend({
-	{type = "int-setting", name = "MUI_update-tick", setting_type = "runtime-global", default_value = 120, minimal_value = 1, maximal_value = 8e4},
+	{type = "int-setting", name = "MUI_update-tick", setting_type = "runtime-global", default_value = 30, minimal_value = 1, maximal_value = 8e4},
 })
