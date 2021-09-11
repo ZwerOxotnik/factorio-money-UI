b37df36a949d542dfb36c0a775e214fec1c5ffda
ZwerOxotnik <zweroxotnik@gmail.com>
2021-09-11 16:03:52 +0300

    Update to 0.6.3

--- settings.lua
+++ settings.lua
@@ -1,3 +1,7 @@
 data:extend({
-	{type = "int-setting", name = "MUI_update-tick", setting_type = "runtime-global", default_value = 30, minimal_value = 1, maximal_value = 8e4},
+	{
+		type = "int-setting", name = "MUI_update-tick",
+		setting_type = "runtime-global", localised_name = {"mod-setting-name.update-tick"},
+		default_value = 30, minimal_value = 1, maximal_value = 8e4
+	}
 })
