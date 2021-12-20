88111d61d6e105158137b5eb0e62e19dcdcfeae2
ZwerOxotnik <zweroxotnik@gmail.com>
2021-12-20 19:33:40 +0300

    Update to 0.8.0

--- models/money-UI.lua
+++ models/money-UI.lua
@@ -2,9 +2,20 @@
 local M = {}
 
 
+--#region Global data
+---@class mod_data
+local mod_data
+
+---@class opened_money_UI
+---@type table
+local opened_money_UI
+--#endregion
+
+
 --#region Constants
 local call = remote.call
-local anchor = {gui = defines.relative_gui_type.controller_gui, position = defines.relative_gui_position.top}
+local money_anchor = {gui = defines.relative_gui_type.controller_gui, position = defines.relative_gui_position.top}
+local controller_type = defines.gui_type.controller
 --#endregion
 
 
@@ -17,7 +28,7 @@ local update_tick = settings.global["MUI_update-tick"].value
 
 local function create_relative_gui(player)
 	local relative = player.gui.relative
-	local main_frame = relative.add{type = "frame", name = "money_frame", anchor = anchor}
+	local main_frame = relative.add{type = "frame", name = "money_frame", anchor = money_anchor}
 	main_frame.style.vertical_align = "center"
 	main_frame.style.horizontally_stretchable = false
 	main_frame.style.bottom_margin = -14
@@ -42,6 +53,38 @@ local function on_player_created(event)
 	create_relative_gui(game.get_player(event.player_index))
 end
 
+local function on_player_changed_force(event)
+	local player_data = opened_money_UI[event.player_index]
+	if player_data == nil then return end
+
+	local force_index = game.get_player(event.player_index).force.index
+	player_data[3] = force_index
+end
+
+local function on_gui_opened(event)
+	if event.gui_type ~= controller_type then return end
+
+	local player = game.get_player(event.player_index)
+	if player.opened_self then
+		local gui = player.gui.relative.money_frame.content.table
+		opened_money_UI[player.index] = {
+			gui.player_balance,
+			gui.force_balance,
+			player.force.index
+		}
+	end
+end
+
+local function on_gui_closed(event)
+	if event.gui_type ~= controller_type then return end
+
+	local player = game.get_player(event.player_index)
+	if not player.opened_self then
+		opened_money_UI[player.index] = nil
+	end
+end
+
+-- Perhaps, I should refactor it
 local function on_gui_click(event)
 	if event.element.name ~= "MUI_money" then return end
 
@@ -53,7 +96,7 @@ local function on_gui_click(event)
 	else
 		table.player_balance.caption = "NaN"
 	end
-	local force_index = game.get_player(event.player_index).force.index
+	local force_index = game.get_player(player_index).force.index
 	local force_money = call("EasyAPI", "get_force_money", force_index)
 	if force_money then
 		table.force_balance.caption = tostring(force_money)
@@ -63,19 +106,15 @@ local function on_gui_click(event)
 end
 
 local function check_GUIs()
+	if #opened_money_UI == 0 then return end
+
 	local forces_money = call("EasyAPI", "get_forces_money")
 	local players_money = call("EasyAPI", "get_online_players_money")
-	for _, force in pairs(game.forces) do
-		local force_money = tostring(forces_money[force.index] or "NaN")
-		local online_players = force.connected_players
-		for i=1, #online_players do
-			local player = online_players[i]
-			if player.opened_self then
-				local table = player.gui.relative.money_frame.content.table
-				table.player_balance.caption = tostring(players_money[player.index] or "NaN")
-				table.force_balance.caption = force_money
-			end
-		end
+
+	for player_index, data in pairs(opened_money_UI) do
+		local force_money = tostring(forces_money[data[3]] or "NaN")
+		data[1].caption = tostring(players_money[player_index] or "NaN")
+		data[2].caption = force_money
 	end
 end
 
@@ -93,6 +132,19 @@ end
 
 --#region Pre-game stage
 
+local function link_data()
+	mod_data = global.MUI
+	opened_money_UI = mod_data.opened_money_UI
+end
+
+local function update_global_data()
+	global.MUI = global.MUI or {}
+	mod_data = global.MUI
+	mod_data.opened_money_UI = mod_data.opened_money_UI or {}
+
+	link_data()
+end
+
 local function add_remote_interface()
 	-- https://lua-api.factorio.com/latest/LuaRemote.html
 	remote.remove_interface("money-UI") -- For safety
@@ -100,19 +152,53 @@ local function add_remote_interface()
 end
 
 M.on_init = function()
+	update_global_data()
+
 	for _, player in pairs(game.players) do
 		if player.valid then
 			create_relative_gui(player)
 		end
 	end
 end
+M.on_configuration_changed = function(event)
+	update_global_data()
+
+	-- local mod_changes = event.mod_changes["money-UI"]
+	-- if not (mod_changes and mod_changes.old_version) then return end
+
+	-- local version = tonumber(string.gmatch(mod_changes.old_version, "%d+.%d+")())
+	-- if version < 0.8 then
+	-- end
+end
+M.on_load = link_data
 M.add_remote_interface = add_remote_interface
 
 --#endregion
 
 
 M.events = {
-	[defines.events.on_player_created] = on_player_created,
+	[defines.events.on_player_created] = function(event)
+		pcall(on_player_created, event)
+	end,
+	[defines.events.on_player_changed_force] = function(event)
+		pcall(on_player_changed_force, event)
+	end,
+	[defines.events.on_player_left_game] = function(event)
+		opened_money_UI[event.player_index] = nil
+	end,
+	[defines.events.on_player_joined_game] = function()
+		-- TODO: improve
+		if #game.connected_players == 1 then
+			mod_data.opened_money_UI = {}
+			opened_money_UI = mod_data.opened_money_UI
+		end
+	end,
+	[defines.events.on_gui_opened] = function(event)
+		pcall(on_gui_opened, event)
+	end,
+	[defines.events.on_gui_closed] = function(event)
+		pcall(on_gui_closed, event)
+	end,
 	[defines.events.on_gui_click] = function(event)
 		pcall(on_gui_click, event)
 	end,
